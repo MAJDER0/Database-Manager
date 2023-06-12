690f6375f0d8703cf6d71ddab1cf14fd803f747f
MAJDER0 <tomasz.majder89@gmail.com>
2023-06-12 22:46:38 +0200

    Connecting Database

---
+++ Database Manager/Zwierzetum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Database_Manager;
+
+public partial class Zwierzetum
+{
+    public int Id { get; set; }
+
+    public string Nazwa { get; set; } = null!;
+
+    public string Typ { get; set; } = null!;
+
+    public string SposobOdzywiania { get; set; } = null!;
+
+    public virtual Gady? Gady { get; set; }
+
+    public virtual Plazy? Plazy { get; set; }
+
+    public virtual Ptaki? Ptaki { get; set; }
+
+    public virtual Ryby? Ryby { get; set; }
+
+    public virtual Ssaki? Ssaki { get; set; }
+}
