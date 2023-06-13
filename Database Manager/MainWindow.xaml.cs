9d0fd25d75a5f54686348d885a0368c7e774dad8
MAJDER0 <tomasz.majder89@gmail.com>
2023-06-14 01:52:49 +0200

    UI design and add record logic

--- Database Manager/MainWindow.xaml.cs
+++ Database Manager/MainWindow.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
@@ -20,17 +21,81 @@ namespace Database_Manager
     /// </summary>
     public partial class MainWindow : Window
     {
-        public List<Gady>gady { get; set; }
+        public List<Zwierzetum>zwierzeta { get; set; }
 
         public MainWindow()
         {
             InitializeComponent();
+
             using (ZooContext _context = new ZooContext())
             {
-                gady = _context.Gadies.ToList();
+                zwierzeta = _context.Zwierzeta.ToList();
             }
 
-            Gady.ItemsSource = gady;
+            Animals.ItemsSource = zwierzeta;
+        }
+
+        private void Add_Record_Click(object sender, RoutedEventArgs e)
+        {
+            using (ZooContext _context = new ZooContext())
+            {
+                string typ = Typ.Text.ToLower();
+                string nazwaZwierzecia = NazwaZwierzecia.Text.ToLower();
+                string sposobOdzywiania = SposobOdzywiania.Text.ToLower();
+
+                Zwierzetum newZwierze = new Zwierzetum()
+                {
+                    Nazwa = nazwaZwierzecia,
+                    Typ = typ,
+                    SposobOdzywiania = sposobOdzywiania
+                };
+
+                // Add the new Gady object to the DbSet
+                _context.Zwierzeta.Add(newZwierze);
+
+                switch (typ)
+                {
+                    case "ssak":
+                        Ssaki newSsak = new Ssaki
+                        {
+                            Nazwa = nazwaZwierzecia,
+                            SposobOdzywiania = sposobOdzywiania
+
+                        };
+                        _context.Ssakis.Add(newSsak);
+                        break;
+
+                    case "gad":
+                        Gady newGad = new Gady
+                        {
+                            Nazwa = nazwaZwierzecia,
+                            SposobOdzywiania = sposobOdzywiania
+                        };
+                        _context.Gadies.Add(newGad);
+                        break;
+
+                    case "ptak":
+                        Ptaki newPtak = new Ptaki
+                        {
+                            Nazwa = nazwaZwierzecia,
+                            SposobOdzywiania = sposobOdzywiania
+                        };
+                        _context.Ptakis.Add(newPtak);
+                        break;
+
+                    case "plaz":
+                        Plazy newPlaz = new Plazy
+                        {
+                            Nazwa = nazwaZwierzecia,
+                            SposobOdzywiania = sposobOdzywiania
+                        };
+                        _context.Plazies.Add(newPlaz);
+                        break;
+                }
+
+                _context.SaveChanges();
+            }
+
         }
     }
 }
--- Database Manager/Ssaki.cs
+++ Database Manager/Ssaki.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace Database_Manager;
 
 public partial class Ssaki
 {
+
     public int ZwierzeId { get; set; }
 
     public string Nazwa { get; set; } = null!;
