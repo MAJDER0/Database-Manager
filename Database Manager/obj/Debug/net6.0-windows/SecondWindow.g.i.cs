2df4ff30974ae27b812312263fd881d845a7c72f
MAJDER0 <tomasz.majder89@gmail.com>
2023-06-16 00:31:31 +0200

    fixing stuff

--- Database Manager/obj/Debug/net6.0-windows/SecondWindow.g.i.cs
+++ Database Manager/obj/Debug/net6.0-windows/SecondWindow.g.i.cs
@@ -77,7 +77,7 @@ namespace Database_Manager {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Database Manager;component/secondwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Database Manager;V1.0.0.0;component/secondwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\SecondWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
